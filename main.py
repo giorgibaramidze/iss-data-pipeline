4fdf2f6671178fc0964a397db5096b0ac30c2674
Barama <baramidze1998@gmail.com>
2026-09-24 17:53:49 +0400

    add while loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from database import Database
 from storage import JSONStorage
 from services import ISSCollector
@@ -14,6 +15,7 @@ from config import (
 
 def main():
     logger.info("Starting ISS tracking pipeline execution...")
+    
     try:
         db_action = Database(db_connection)
         db_action.create_database()
@@ -23,32 +25,39 @@ def main():
         iss_client = ISSAPIClient(ISS_API)
         geocoder_client = GeocoderClient(OPENCAGE_API_KEY)
         collector = ISSCollector(iss_client, geocoder_client, storage)
-
-        iss_data, location = collector.collect_data()
-
-        distance_km = db_action.fetch_traveled_distance(current_rec=iss_data)
-        iss_id = db_action.insert_into_iss_data(current_rec=iss_data)
-        db_action.insert_into_iss_enriched(
-            iss_id=iss_id, location=location, distance_km=distance_km
-        )
-
-        place_name = (
-            location.get("city") 
-            or location.get("country") 
-            or location.get("body_of_water") 
-            or "Unknown Location"
-        )
-        velocity = iss_data.get("velocity")
-
-        logger.info(
-            f"SUMMARY: ISS Record #{iss_id} | "
-            f"Location: {place_name} | "
-            f"Distance since last reading: {distance_km:.2f} km | "
-            f"Velocity: {velocity:.2f} km/h"
-        )
-
     except Exception as e:
-        logger.error(f"Pipeline execution failed: {e}")
+        logger.error(f"Pipeline initialization failed: {e}")
+        return
+
+    while True:
+        try:
+            iss_data, location = collector.collect_data()
+
+            distance_km = db_action.fetch_traveled_distance(current_rec=iss_data)
+            iss_id = db_action.insert_into_iss_data(current_rec=iss_data)
+            db_action.insert_into_iss_enriched(
+                iss_id=iss_id, location=location, distance_km=distance_km
+            )
+
+            place_name = (
+                location.get("city") 
+                or location.get("country") 
+                or location.get("body_of_water") 
+                or "Unknown Location"
+            )
+            velocity = iss_data.get("velocity")
+
+            logger.info(
+                f"SUMMARY: ISS Record #{iss_id} | "
+                f"Location: {place_name} | "
+                f"Distance since last reading: {distance_km:.2f} km | "
+                f"Velocity: {velocity:.2f} km/h"
+            )
+
+        except Exception as e:
+            logger.error(f"Pipeline execution step failed: {e}")
+
+        time.sleep(50)
 
 
 if __name__ == "__main__":
